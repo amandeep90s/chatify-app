dfba214816803b8d46e6be4801fd3fe890522ff4
Amandeep Singh <programmeramandeep@gmail.com>
2025-08-30 23:32:15 +0530

    feat: Update dependencies, refactor App component, and remove unused styles

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { CssBaseline } from "@mui/material";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HelmetProvider>
+      <CssBaseline />
+      <App />
+    </HelmetProvider>
+  </StrictMode>
+);
